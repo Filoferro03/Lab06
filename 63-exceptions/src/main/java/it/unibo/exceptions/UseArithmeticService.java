7142ef00b625a240bc0f9ac9df304e65897c2cdd
Filippo Ferretti <filippo.ferretti6@studio.unibo.it>
2023-11-04 14:57:38 +0100

    Implemented the two functions described in point 2 catch existing exception

--- 63-exceptions/src/main/java/it/unibo/exceptions/UseArithmeticService.java
+++ 63-exceptions/src/main/java/it/unibo/exceptions/UseArithmeticService.java
@@ -3,6 +3,7 @@ package it.unibo.exceptions;
 import it.unibo.exceptions.fakenetwork.api.NetworkComponent;
 import it.unibo.exceptions.fakenetwork.impl.ServiceBehindUnstableNetwork;
 
+import java.io.IOException;
 import java.io.PrintStream;
 
 import static it.unibo.exceptions.arithmetic.ArithmeticService.DIVIDED;
@@ -46,6 +47,17 @@ public final class UseArithmeticService {
          * This method should re-try to send message to the provided server, catching all IOExceptions,
          * until it succeeds.
          */
+        boolean notSucceed = true;
+        while (notSucceed) {
+            try {
+                notSucceed=false;
+                server.sendData(message);
+            } catch (IOException e) {
+                LOG.println(e.getMessage());
+                notSucceed=true;
+            }
+        }
+        
     }
 
     private static String retryReceiveOnNetworkError(final NetworkComponent server) {
@@ -53,6 +65,16 @@ public final class UseArithmeticService {
          * This method should re-try to retrieve information from the provided server, catching all IOExceptions,
          * until it succeeds.
          */
+        boolean notSucceed = true;
+        while (notSucceed) {
+            try {
+                notSucceed=false;
+                return server.receiveResponse();
+            } catch (IOException e) {
+                LOG.println(e.getMessage());
+                notSucceed=true;
+            }
+        }
         return null;
     }
 
